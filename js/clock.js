824540a339bb580aca5958c51bc89c09ae28196e
suminLee <tnalswkd1122@naver.com>
2022-12-08 20:23:16 +0900

    greeting 추가 업로드

--- js/clock.js
+++ js/clock.js
@@ -1,12 +1,14 @@
+// h2 #clock 가져오기
 const clock = document.querySelector('h2#clock');
 
+// 시간을 받아서 h2의 innerText에 업데이트
 function getClock() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
   clock.innerText = `${hours}:${minutes}`;
 }
 
+// 1초마다 getClock() 실행
 getClock();
 setInterval(getClock, 1000);
